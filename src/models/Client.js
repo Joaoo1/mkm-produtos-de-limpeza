70c41480172bd762de15e38a302750615f44195a
João Vitor da Silva <silva.joao1@outlook.com.br>
2020-06-02 23:36:00 -0300

    Ajust client model to avoid empty fields

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,21 +1,21 @@
 export default class Client {
   constructor({ bairro, cidade, endereco, complemento, nome, telefone, id }) {
-    this.bairro = bairro;
-    this.cidade = cidade;
-    this.endereco = endereco;
-    this.complemento = complemento;
-    this.nome = nome;
-    this.telefone = telefone;
     this.id = id;
+    this.nome = nome;
+    if (bairro) this.bairro = bairro;
+    if (cidade) this.cidade = cidade;
+    if (endereco) this.endereco = endereco;
+    if (complemento) this.complemento = complemento;
+    if (telefone) this.telefone = telefone;
   }
 
   cleanUp() {
-    this.bairro = '';
-    this.cidade = '';
-    this.endereco = '';
-    this.complemento = '';
-    this.nome = '';
-    this.telefone = '';
     this.id = '';
+    this.nome = '';
+    if (this.bairro) this.bairro = '';
+    if (this.cidade) this.cidade = '';
+    if (this.endereco) this.endereco = '';
+    if (this.complemento) this.complemento = '';
+    if (this.telefone) this.telefone = '';
   }
 }
